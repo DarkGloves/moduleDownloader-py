3838bc4b671e9711d67e07f0741328fad2c763c9
XploitEmAll!_ <110231449+DarkGloves@users.noreply.github.com>
2023-03-31 13:15:52 -0600

    Add files via upload

---
+++ moduleDownloader.py
@@ -0,0 +1,11 @@
+# Made by DarkGloves
+# This module gets an specified module from pypi in case it's not found
+# You can find more information on https://github.com/DarkGloves/moduleDownloader-py 
+import os
+
+def getmodules(*modules, pipver=int):
+    for module in modules:
+        try:
+           __import__(module)
+        except ModuleNotFoundError:
+            os.system(f'pip{pipver} install {module}')
